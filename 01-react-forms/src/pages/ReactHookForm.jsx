836b33c600ced24798c5a9e82e95f8625b2c6e79
César Guerra <warderer@gmail.com>
2024-11-20 21:45:25 -0600

    ✨ Add ReactHookForm library

--- 01-react-forms/src/pages/ReactHookForm.jsx
+++ 01-react-forms/src/pages/ReactHookForm.jsx
@@ -1,23 +1,33 @@
 import logo from '../assets/react.svg'
+// #1 Importar el hook useForm de react-hook-form
+import { useForm } from 'react-hook-form'
 
 const ReactHookForm = () => {
+    // #2 Crear una instancia de useForm
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    // #3 Definir la función que se ejecutará cuando el usuario haga submit
+    const onSubmit = data => console.log(data)
 
   return (
     <div className='login'>
       <div className='login-container'>
         <img src={logo} alt='logo' />
         <form
-          onSubmit={() => { }/* HANDLE SUBMIT */}
+          onSubmit={handleSubmit(onSubmit)}
           style={{ display: 'flex', flexDirection: 'column' }}
         >
-
+          {/* 4. Registro cada elemento de mi formulario */}
           <label htmlFor='firstName'>Nombre</label>
           <input
             type='text'
             name='firstName'
             placeholder='Tu Nombre'
             id='firstName'
+            {...register('firstName', { required: true })}
           />
+          {/* errors will return when field validation fails  */}
+          {errors.firstName && <span>This field is required</span>}
 
           <label htmlFor='lastName'>Apellido</label>
           <input
@@ -25,6 +35,7 @@ const ReactHookForm = () => {
             name='lastName'
             placeholder='Tu Apellido'
             id='lastName'
+            {...register('lastName')}
           />
 
           <label htmlFor='age'>Edad</label>
@@ -33,10 +44,11 @@ const ReactHookForm = () => {
             name='age'
             placeholder='Tu Edad'
             id='age'
+            {...register('age')}
           />
 
           <label htmlFor='gender'>Genero</label>
-          <select name='gender' id='gender'>
+          <select name='gender' id='gender' {...register('gender')}>
             <option value=''>Elige un genero</option>
             <option value='M'>Masculino</option>
             <option value='F'>Femenino</option>
@@ -49,6 +61,7 @@ const ReactHookForm = () => {
             name='email'
             placeholder='correo@mail.com'
             id='email'
+            {...register('email')}
           />
 
           <label htmlFor='password'>Password</label>
@@ -56,6 +69,7 @@ const ReactHookForm = () => {
             type='password'
             name='password'
             id='password'
+            {...register('password')}
           />
 
           <button type='submit'>
